d23240560b4ed02a89225777a2414862a3da0c1d
Luke Enness <lukeenness@outlook.com>
2025-02-06 20:49:11 +0000

    folder name change

---
+++ src/test.java
@@ -0,0 +1,391 @@
+/*
+CS-256 Getting started code for the assignment
+I do not give you permission to post this code online
+Do not copy code
+Do not use libraries to do the Slicing, MIP or Volume Rendering. That code must be written by yourself
+You may use libraries / IDE to achieve a better GUI
+*/
+import java.io.*;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;  
+import javafx.scene.image.WritableImage;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class test extends Application {
+	short cthead[][][]; //store the 3D volume data set
+	float grey[][][]; //store the 3D volume data set converted to 0-1 ready to copy to the image
+	short min, max; //min/max value in the 3D volume data set
+
+	int currZSlice = 128;
+	int currXSlice = 128;
+	int currYSlice = 128;
+
+    @Override
+    public void start(Stage stage) throws FileNotFoundException {
+		stage.setTitle("CThead Viewer");
+		
+		try {
+			readData();
+		} catch (IOException e) {
+			System.out.println("Error: The CThead file is not in the working directory");
+			System.out.println("Working Directory = " + System.getProperty("user.dir"));
+			return;
+		}
+
+		//We need 3 things to see an image
+		//1. We need to create the image
+		WritableImage sliceZImage = new WritableImage(256, 256); //allocate memory for the image
+		getZSlice(currZSlice, sliceZImage); //make the image - in this case go get the slice and copy it into the image
+		//2. We link a view in the GUI to that image
+		ImageView sliceZView = new ImageView(sliceZImage); //and then see 3. below
+
+		WritableImage sliceXImage = new WritableImage(256, 246);
+		getXSlice(currXSlice, sliceXImage);
+		ImageView sliceXView = new ImageView(sliceXImage);
+
+		WritableImage sliceYImage = new WritableImage(256, 256);
+		getYSlice(currYSlice, sliceYImage);
+		ImageView sliceYView = new ImageView(sliceYImage);
+
+		// Do the same for MIP
+		WritableImage MIPZImage = new WritableImage(256, 256);
+		getZMIP(MIPZImage);
+		ImageView MIPZView = new ImageView(MIPZImage);
+
+		WritableImage MIPXImage = new WritableImage(256, 256);
+		getXMIP(MIPXImage);
+		ImageView MIPXView = new ImageView(MIPXImage);
+
+		WritableImage MIPYImage = new WritableImage(256, 256);
+		getYMIP(MIPYImage);
+		ImageView MIPYView = new ImageView(MIPYImage);
+
+		WritableImage VRZImage = new WritableImage(256, 256);
+		getZVR(VRZImage);
+		ImageView VRZView = new ImageView(VRZImage);
+
+		WritableImage VRXImage = new WritableImage(256, 256);
+		getXVR(VRXImage);
+		ImageView VRXView = new ImageView(VRXImage);
+
+		WritableImage VRYImage = new WritableImage(256, 256);
+		getYVR(VRYImage);
+		ImageView VRYView = new ImageView(VRYImage);
+
+
+		//Create the simple GUI
+		Slider sliceZSlider = new Slider(0, 255, currZSlice);
+		sliceZSlider.valueProperty().addListener(new ChangeListener<Number>() { 
+			public void changed(ObservableValue <? extends Number >  
+					observable, Number oldValue, Number newValue) { 
+
+				currZSlice = newValue.intValue();
+				//System.out.println(currZSlice);
+				//We update our Image
+		        getZSlice(currZSlice, sliceZImage); //go get the slice image
+				//Because sliceZView (an ImageView) is linked to it, this will automatically update the displayed image in the GUI
+            } 
+    	});		
+
+		Slider sliceXSlider = new Slider(0, 255, currXSlice);
+		sliceXSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue <? extends Number >
+					observable, Number oldValue, Number newValue) {
+
+				currXSlice = newValue.intValue();
+				getXSlice(currXSlice, sliceXImage);
+			}
+		});
+
+		Slider sliceYSlider = new Slider(0, 255, currYSlice);
+		sliceYSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue <? extends Number >
+					observable, Number oldValue, Number newValue) {
+
+				currYSlice = newValue.intValue();
+				getYSlice(currYSlice, sliceYImage);
+			}
+		});
+
+		Slider opacitySlider = new Slider(0, 100, 50);
+		opacitySlider.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue <? extends Number >
+					observable, Number oldValue, Number newValue) {
+
+				// Implement opacity change here
+				
+
+			}
+		});
+
+		//Add all the GUI elements
+		//I'll start a grid for you
+		GridPane grid = new GridPane();
+        grid.add(sliceZSlider, 0, 0); // Slider at column 0, row 0
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        //3. (referring to the 3 things we need to display an image)
+      	//we need to add it to the grid
+		grid.add(sliceZView, 0, 1); // Slider at column 0, row 1
+		grid.add(MIPZView, 0, 2); // Slider at column 0, row 1
+		grid.add(VRZView, 0, 3);
+		
+		grid.add(sliceXSlider, 1, 0);
+		grid.add(sliceXView, 1, 1);
+		grid.add(MIPXView, 1, 2);
+		grid.add(VRXView, 1, 3);
+
+		grid.add(sliceYSlider, 2, 0);
+		grid.add(sliceYView, 2, 1);
+		grid.add(MIPYView, 2, 2);
+		grid.add(VRYView, 2, 3);
+
+		grid.add(opacitySlider, 1, 4);
+
+		// Create a scene and set the stage
+        Scene scene = new Scene(grid, 800, 840);
+        stage.setTitle("CT Data Viewer");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+
+	//Function to read in the cthead data set
+	public void readData() throws IOException {
+		//If you've put the test.java in a directory called "src" and put the dataset in the parent directory, then this will be the correct path
+		File file = new File("CThead-256cubed.bin");
+		//Read the data quickly via a buffer (in C++ you can just do a single fread - I couldn't find the equivalent in Java)
+		DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+		
+		int i, j, k; //loop through the 3D data set
+		
+		min=Short.MAX_VALUE; max=Short.MIN_VALUE; //set to extreme values
+		short read; //value read in
+		int b1, b2; //data is wrong Endian (check wikipedia) for Java so we need to swap the bytes around
+		
+		cthead = new short[256][256][256]; //allocate the memory - note this is fixed for this data set
+		grey= new float[256][256][256];
+		//loop through the data reading it in
+		for (k=0; k<256; k++) {
+			for (j=0; j<256; j++) {
+				for (i=0; i<256; i++) {
+					//because the Endianess is wrong, it needs to be read byte at a time and swapped
+					b1=((int)in.readByte()) & 0xff; //the 0xff is because Java does not have unsigned types (C++ is so much easier!)
+					b2=((int)in.readByte()) & 0xff; //the 0xff is because Java does not have unsigned types (C++ is so much easier!)
+					read=(short)((b2<<8) | b1); //and swizzle the bytes around
+					if (read<min) min=read; //update the minimum
+					if (read>max) max=read; //update the maximum
+					cthead[k][j][i]=read; //put the short into memory (in C++ you can replace all this code with one fread)
+				}
+			}
+		}
+		System.out.println(min+" "+max); //diagnostic - for CThead-256cubed.bin this should be -1897, 3029
+		//(i.e. there are 4927 levels of grey, and now we will normalise them to 0-1 for display purposes
+		//I know the min and max already, so I could have put the normalisation in the above loop, but I put it separate here
+		for (k=0; k<256; k++) {
+			for (j=0; j<256; j++) {
+				for (i=0; i<256; i++) {
+					grey[k][j][i]=((float) cthead[k][j][i]-(float) min)/((float) max-(float) min);
+				}
+			}
+		}
+		//At this point, cthead is the original dataset
+		//and grey is 0-1 float data that can be displayed by Java
+	}
+
+
+
+	public void getZSlice(int slice, WritableImage image) {
+		//Find the width and height of the image to be process
+		int width = (int)image.getWidth();
+        int height = (int)image.getHeight();
+		float val;
+
+		//Get an interface to write to that image memory
+		PixelWriter image_writer = image.getPixelWriter();
+
+		//Iterate over all pixels
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				//I'm going to get the middle slice as an example
+				val = grey[slice][y][x];
+
+				//Or uncomment this to make a grey image dependent on the slider value so you can see how the GUI updates
+				//val = (float) slice / 255.f;
+
+				Color color=Color.color(val, val, val);
+				//Apply the new colour
+				image_writer.setColor(x, y, color);
+			}
+		}
+	}
+
+	public void getZMIP(WritableImage image) {
+		//Find the width and height of the image to be process
+		int width = (int)image.getWidth();
+        int height = (int)image.getHeight();
+
+		//Get an interface to write to that image memory
+		PixelWriter image_writer = image.getPixelWriter();
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+
+				float maxSoFar = min;
+				for (int z = 0; z < 256; z++) {
+					maxSoFar = Math.max(grey[z][y][x], maxSoFar);
+				}
+				
+				Color color = Color.color(maxSoFar, maxSoFar, maxSoFar);
+				image_writer.setColor(x, y, color);
+			}
+		}
+	}
+
+	public void getZVR(WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+
+		PixelWriter image_writer = image.getPixelWriter();
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				//Implement Volume Rendering here
+				
+
+				
+				
+				Color color=Color.color(1.0, 1.0, 1.0);
+				image_writer.setColor(x, y, color);
+			}
+		}
+	}
+
+	
+
+	public void getXSlice(int slice, WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+		float val;
+	
+		PixelWriter image_writer = image.getPixelWriter();
+	
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				val = grey[y][x][slice];
+	
+				Color color=Color.color(val, val, val);
+				image_writer.setColor(x, y, color);
+			}
+		}
+	}
+
+	public void getXMIP(WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+		
+		PixelWriter image_writer = image.getPixelWriter();
+		
+		for (int y = 0; y < height; y++) {
+			for (int z = 0; z < width; z++) {
+				
+				float maxSoFar = min;
+				for (int x = 0; x < 256; x++) {
+					maxSoFar = Math.max(grey[y][z][x], maxSoFar);
+				}
+
+				Color color=Color.color(maxSoFar, maxSoFar, maxSoFar);
+				image_writer.setColor(z, y, color);
+			}
+		}
+	}
+
+	public void getXVR(WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+		
+		PixelWriter image_writer = image.getPixelWriter();
+		
+		for (int y = 0; y < height; y++) {
+			for (int z = 0; z < width; z++) {
+				//Implement Volume Rendering here
+				
+
+				
+				Color color=Color.color(1.0, 1.0, 1.0);
+				image_writer.setColor(z, y, color);
+			}
+		}
+	}
+
+
+
+	public void getYSlice(int slice, WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+		float val;
+		
+		PixelWriter image_writer = image.getPixelWriter();
+		
+		for (int y = 0; y < height; y++) { 
+			for (int x = 0; x < width; x++) {
+				val = grey[y][slice][x];
+
+				Color color=Color.color(val, val, val);
+				image_writer.setColor(x, y, color);
+			}
+		}
+	}
+
+	public void getYMIP(WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+	
+		PixelWriter image_writer = image.getPixelWriter();
+	
+		for (int z = 0; z < width; z++) {
+			for (int x = 0; x < height; x++) {
+
+				float maxSoFar = min;
+				for (int y = 0; y < 256; y++) {
+					maxSoFar = Math.max(grey[z][y][x], maxSoFar);
+				}
+
+				Color color=Color.color(maxSoFar, maxSoFar, maxSoFar);
+				image_writer.setColor(x, z, color);
+			}
+		}
+	}
+
+	public void getYVR(WritableImage image) {
+		int width = (int)image.getWidth();
+		int height = (int)image.getHeight();
+	
+		PixelWriter image_writer = image.getPixelWriter();
+	
+		for (int z = 0; z < width; z++) {
+			for (int x = 0; x < height; x++) {
+				//Implement Volume Rendering here
+				
+
+				
+				Color color=Color.color(1.0, 1.0, 1.0);
+				image_writer.setColor(z, x, color);
+			}
+		}
+	}
+
+	
+	
+    public static void main(String[] args) {
+		launch();
+    }
+}
